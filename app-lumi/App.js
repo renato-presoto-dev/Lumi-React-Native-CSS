1c622fb96f0290786b37945730516b4ca533070b
Renato Presoto <renatopresoto.dev@gmail.com>
2026-02-17 18:35:48 -0300

    002:Rotas de navegação para paginas

--- app-lumi/App.js
+++ app-lumi/App.js
@@ -2,9 +2,11 @@
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import Home from './src/pages/Home'
-import Lista from './src/pages/Lista'
-import CriarLista from './src/pages/CriarLista'
-
+import Config from './src/pages/Config'
+import Learn from './src/pages/Learn'
+import Question from './src/pages/Question'
+import Statistic from './src/pages/Statistic'
+import Welcome from './src/pages/Welcome'
 
 const Stack = createStackNavigator();
 
@@ -13,8 +15,11 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator intialRouteName="Home">
         <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
-        <Stack.Screen name="CriarLista" component={CriarLista} />
-        <Stack.Screen name="Lista" component={Lista}/>
+        <Stack.Screen name="Config" component={Config} options={{headerShown:false}}/>
+        <Stack.Screen name="Learn" component={Learn} options={{headerShown:false}}/>
+        <Stack.Screen name="Question" component={Question} options={{headerShown:false}}/>
+        <Stack.Screen name="Statistic" component={Statistic} options={{headerShown:false}}/>
+        <Stack.Screen name="Welcome" component={Welcome} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
 
